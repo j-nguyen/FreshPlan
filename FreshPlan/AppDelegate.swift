c8a6b79cb24c68a654c8ca710c0fd5e42ca5ae11
Johnny Nguyen <jnguyen1236@gmail.com>
2017-10-05 14:48:07 -0400

    Added gitignore

--- FreshPlan/AppDelegate.swift
+++ FreshPlan/AppDelegate.swift
@@ -13,9 +13,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 	var window: UIWindow?
 
-
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
+		
+		// setup window to make sure
+		window?.rootViewController = ViewController()
+		window?.makeKeyAndVisible()
+		window?.backgroundColor = UIColor.white
+		
 		return true
 	}
 
--- FreshPlan/ViewController.swift
+++ FreshPlan/ViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+public class ViewController: UIViewController {
+	
+	public convenience init() {
+		self.init(nibName: nil, bundle: nil)
+	}
+	
+	public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+	}
 
-	override func viewDidLoad() {
+	public required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+	
+	public override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	
+		// prepare any other views here
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
-	}
-
-
 }
 
