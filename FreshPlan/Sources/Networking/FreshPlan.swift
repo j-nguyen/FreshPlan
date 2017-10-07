c75b51ea58fcdace9208c29fb29de67c968d02e4
Johnny Nguyen <jnguyen1236@gmail.com>
2017-10-07 17:17:27 -0400

    Removed pods from gitignore

---
+++ FreshPlan/Sources/Networking/FreshPlan.swift
@@ -0,0 +1,10 @@
+//
+//  FreshPlan.swift
+//  FreshPlan
+//
+//  Created by Johnny Nguyen on 2017-10-07.
+//  Copyright © 2017 St Clair College. All rights reserved.
+//
+
+import Moya
+
--- Pods/Target Support Files/Pods-FreshPlan/Pods-FreshPlan-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FreshPlan : NSObject
-@end
-@implementation PodsDummy_Pods_FreshPlan
-@end
