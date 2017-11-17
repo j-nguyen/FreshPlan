5a4abf96afe84316a0c7165f390aa0b7b6a64b62
Allan Lin <allan.lin15@stclairconnect.ca>
2017-11-16 22:02:32 -0500

    add the sign up button

--- FreshPlan/Sources/Modules/Register/RegisterViewController.swift
+++ FreshPlan/Sources/Modules/Register/RegisterViewController.swift
@@ -16,6 +16,9 @@ public final class RegisterViewController: UIViewController {
 	private var router: RegisterRouter!
 	private var viewModel: RegisterViewModelProtocol!
     
+    // MARK - Labels
+    private var loginInLabel: UILabel!
+    
     // MARK - TextFields
     private var firstNameField: MDCTextField!
     private var lastNameField: MDCTextField!
@@ -29,6 +32,9 @@ public final class RegisterViewController: UIViewController {
     private var displayNameFieldController: MDCTextInputControllerDefault!
     private var emailFieldController: MDCTextInputControllerDefault!
     private var passwordFieldController: MDCTextInputControllerDefault!
+    
+    // MARK - Buttons
+    private var signUpButton: MDCButton!
 	
     // loads the view
     public override func viewDidLoad() {
@@ -38,14 +44,39 @@ public final class RegisterViewController: UIViewController {
     
     //
     fileprivate func prepareView() {
+        prepareLoginInLabel()
         prepareFirstName()
         prepareLastName()
         prepareDisplayName()
         prepareEmail()
         preparePassword()
+        prepareSignUpButton()
+        
     }
     
-    //
+    fileprivate func prepareLoginInLabel() {
+        loginInLabel = UILabel()
+        let registerText = "Already have an account? Login In here!"
+        let mutableString = NSMutableAttributedString(attributedString: NSAttributedString(string: registerText))
+        
+        mutableString.addAttribute(
+            NSForegroundColorAttributeName,
+            value: MDCPalette.lightBlue.accent700!,
+            range: NSRange(location: 24, length: 15)
+        )
+        
+        loginInLabel.attributedText = mutableString
+        loginInLabel.font = UIFont(name: "Helvetica Neue", size: 11)
+        loginInLabel.isUserInteractionEnabled = true
+        
+        view.addSubview(loginInLabel)
+        
+        loginInLabel.snp.makeConstraints { make in
+            make.bottom.equalTo(view).offset(-10)
+            make.centerX.equalTo(view)
+        }
+    }
+    
     fileprivate func prepareFirstName() {
         firstNameField = MDCTextField()
         firstNameField.placeholder = "First Name"
@@ -101,6 +132,7 @@ public final class RegisterViewController: UIViewController {
     fileprivate func prepareEmail() {
         emailField = MDCTextField()
         emailField.placeholder = "Email"
+        emailField.keyboardType = .emailAddress
         emailField.returnKeyType = .next
         
         emailFieldController = MDCTextInputControllerDefault(textInput: emailField)
@@ -119,7 +151,8 @@ public final class RegisterViewController: UIViewController {
     fileprivate func preparePassword() {
         passwordField = MDCTextField()
         passwordField.placeholder = "Password"
-        passwordField.returnKeyType = .next
+        passwordField.isSecureTextEntry = true
+        passwordField.returnKeyType = .done
         
         passwordFieldController = MDCTextInputControllerDefault(textInput: passwordField)
         
@@ -135,6 +168,20 @@ public final class RegisterViewController: UIViewController {
         
     }
     
+    fileprivate func prepareSignUpButton() {
+        signUpButton = MDCButton()
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.backgroundColor = MDCPalette.lightBlue.tint800
+        
+        view.addSubview(signUpButton)
+        
+        signUpButton.snp.makeConstraints { make in
+            make.top.equalTo(passwordField.snp.bottom).offset(15)
+            make.left.equalTo(10)
+            make.right.equalTo(-10)
+            make.width.equalTo(300)
+        }
+    }
     
 	public convenience init(router: RegisterRouter, viewModel: RegisterViewModel) {
 		self.init(nibName: nil, bundle: nil)
