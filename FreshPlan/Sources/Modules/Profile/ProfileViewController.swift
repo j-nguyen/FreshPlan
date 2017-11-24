cb20d3123930da13409a248197618d9d700bf3b7
Johnny Nguyen <jnguyen1236@gmail.com>
2017-11-24 12:15:17 -0500

    Fixed indentation

--- FreshPlan/Sources/Modules/Profile/ProfileViewController.swift
+++ FreshPlan/Sources/Modules/Profile/ProfileViewController.swift
@@ -13,140 +13,128 @@ import RxDataSources
 import MaterialComponents
 
 public final class ProfileViewController: UIViewController {
-	//: MARK - Profile View Model and Router
-	private var viewModel: ProfileViewModelProtocol!
-	private var router: ProfileRouter!
-	
-	//: MARK - AppBar
-	fileprivate let appBar: MDCAppBar = MDCAppBar()
-	
-	//: MARK - DisposeBag
-	private let disposeBag: DisposeBag = DisposeBag()
-	
-	//: MARK - TableView
-	private var profileTableView: UITableView!
-	private var dataSource: RxTableViewSectionedReloadDataSource<ProfileViewModel.SectionModel>!
-	
-	public convenience init(viewModel: ProfileViewModel, router: ProfileRouter) {
-		self.init(nibName: nil, bundle: nil)
-		self.viewModel = viewModel
-		self.router = router
-		
-		addChildViewController(appBar.headerViewController)
-	}
-	
-	public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-	}
-	
-	public required init?(coder aDecoder: NSCoder) {
-		super.init(coder: aDecoder)
-	}
-	
-	public override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(animated)
-		
-		navigationController?.setNavigationBarHidden(true, animated: animated)
-	}
-	
-	public override var childViewControllerForStatusBarStyle: UIViewController? {
-		return appBar.headerViewController
-	}
-	
-	public override func viewDidLoad() {
-		super.viewDidLoad()
-		prepareView()
-	}
-	
-	private func prepareView() {
-		prepareProfileTableView()
-		prepareNavigationBar()
-		appBar.addSubviewsToParent()
-	}
-	
-	private func prepareNavigationBar() {
-		appBar.headerViewController.headerView.backgroundColor = MDCPalette.blue.tint700
-		appBar.headerViewController.headerView.trackingScrollView = self.profileTableView
-		appBar.navigationBar.tintColor = UIColor.white
-		appBar.navigationBar.titleTextAttributes = [ NSAttributedStringKey.foregroundColor: UIColor.white ]
-		
-		Observable.just("Profile")
-			.bind(to: navigationItem.rx.title)
-			.disposed(by: disposeBag)
-		
-		appBar.navigationBar.observe(navigationItem)
-	}
-	
-	private func prepareProfileTableView() {
-		profileTableView = UITableView()
-		profileTableView.estimatedRowHeight = 100
-		profileTableView.rowHeight = UITableViewAutomaticDimension
-		profileTableView.separatorStyle = .none
-		profileTableView.rx.setDelegate(self).disposed(by: disposeBag)
-		profileTableView.registerCell(ProfileUserHeaderCell.self)
-		profileTableView.registerCell(ProfileUserInfoCell.self)
-	
-		view.addSubview(profileTableView)
-		
-		profileTableView.snp.makeConstraints { $0.edges.equalTo(view) }
-		
-		// set up data sources
-		dataSource = RxTableViewSectionedReloadDataSource<ProfileViewModel.SectionModel>(configureCell: { (dataSource, table, index, _) in
-			switch dataSource[index] {
-			case let .profile(_, profileURL, fullName):
-				let cell = table.dequeueCell(ofType: ProfileUserHeaderCell.self, for: index)
-				cell.fullName.on(.next(fullName))
-				cell.profileURL.on(.next(profileURL))
-				return cell
-			case let .displayName(_, name):
-				let cell = table.dequeueCell(ofType: ProfileUserInfoCell.self, for: index)
-				cell.textLabel?.text = name
-				return cell
-			case let .email(_, description):
-				let cell = table.dequeueCell(ofType: ProfileUserInfoCell.self, for: index)
-				cell.textLabel?.text = description
-				return cell
-			default:
-				return UITableViewCell()
-			}
-		})
-		
-		dataSource.titleForHeaderInSection = { dataSource, index in
-			return dataSource[index].title
-		}
-		
-		viewModel.profileItems
-			.asObservable()
-			.bind(to: profileTableView.rx.items(dataSource: dataSource))
-			.disposed(by: disposeBag)
-	}
+  //: MARK - Profile View Model and Router
+  private var viewModel: ProfileViewModelProtocol!
+  private var router: ProfileRouter!
+  
+  //: MARK - AppBar
+  fileprivate let appBar: MDCAppBar = MDCAppBar()
+  
+  //: MARK - DisposeBag
+  private let disposeBag: DisposeBag = DisposeBag()
+  
+  //: MARK - TableView
+  private var profileTableView: UITableView!
+  private var dataSource: RxTableViewSectionedReloadDataSource<ProfileViewModel.SectionModel>!
+  
+  public convenience init(viewModel: ProfileViewModel, router: ProfileRouter) {
+    self.init(nibName: nil, bundle: nil)
+    self.viewModel = viewModel
+    self.router = router
+    
+    addChildViewController(appBar.headerViewController)
+  }
+  
+  public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+  }
+  
+  public required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+  
+  public override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    
+    navigationController?.setNavigationBarHidden(true, animated: animated)
+  }
+  
+  public override var childViewControllerForStatusBarStyle: UIViewController? {
+    return appBar.headerViewController
+  }
+  
+  public override func viewDidLoad() {
+    super.viewDidLoad()
+    prepareView()
+  }
+  
+  private func prepareView() {
+    prepareProfileTableView()
+    prepareNavigationBar()
+    appBar.addSubviewsToParent()
+  }
+  
+  private func prepareNavigationBar() {
+    appBar.headerViewController.headerView.backgroundColor = MDCPalette.blue.tint700
+    appBar.navigationBar.tintColor = UIColor.white
+    appBar.headerViewController.headerView.maximumHeight = 76.0
+    appBar.navigationBar.titleTextAttributes = [ NSAttributedStringKey.foregroundColor: UIColor.white ]
+    
+    Observable.just("Profile")
+      .bind(to: navigationItem.rx.title)
+      .disposed(by: disposeBag)
+    
+    appBar.navigationBar.observe(navigationItem)
+  }
+  
+  private func prepareProfileTableView() {
+    profileTableView = UITableView()
+    profileTableView.estimatedRowHeight = 100
+    profileTableView.rowHeight = UITableViewAutomaticDimension
+    profileTableView.separatorStyle = .none
+    profileTableView.rx.setDelegate(self).disposed(by: disposeBag)
+    profileTableView.registerCell(ProfileUserHeaderCell.self)
+    profileTableView.registerCell(ProfileUserInfoCell.self)
+    
+    view.addSubview(profileTableView)
+    
+    profileTableView.snp.makeConstraints { $0.edges.equalTo(view) }
+    
+    appBar.headerViewController.headerView.trackingScrollView = profileTableView
+    
+    // set up data sources
+    dataSource = RxTableViewSectionedReloadDataSource<ProfileViewModel.SectionModel>(configureCell: { (dataSource, table, index, _) in
+      switch dataSource[index] {
+      case let .profile(_, profileURL, fullName):
+        let cell = table.dequeueCell(ofType: ProfileUserHeaderCell.self, for: index)
+        cell.fullName.on(.next(fullName))
+        cell.profileURL.on(.next(profileURL))
+        return cell
+      case let .displayName(_, name):
+        let cell = table.dequeueCell(ofType: ProfileUserInfoCell.self, for: index)
+        cell.textLabel?.text = name
+        return cell
+      case let .email(_, description):
+        let cell = table.dequeueCell(ofType: ProfileUserInfoCell.self, for: index)
+        cell.textLabel?.text = description
+        return cell
+      default:
+        return UITableViewCell()
+      }
+    })
+    
+    dataSource.titleForHeaderInSection = { dataSource, index in
+      return index == 0 ? "" : dataSource.sectionModels[index].title
+    }
+    
+    viewModel.profileItems
+      .asObservable()
+      .bind(to: profileTableView.rx.items(dataSource: dataSource))
+      .disposed(by: disposeBag)
+  }
 }
 
 // MARK: UIScrollViewDelegate
 extension ProfileViewController: UITableViewDelegate {
-	public func scrollViewDidScroll(_ scrollView: UIScrollView) {
-		if scrollView == appBar.headerViewController.headerView.trackingScrollView {
-			appBar.headerViewController.headerView.trackingScrollDidScroll()
-		}
-	}
-
-	public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
-		if scrollView == appBar.headerViewController.headerView.trackingScrollView {
-			appBar.headerViewController.headerView.trackingScrollDidEndDecelerating()
-		}
-	}
-
-	public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
-		let headerView = appBar.headerViewController.headerView
-		if scrollView == headerView.trackingScrollView {
-			headerView.trackingScrollDidEndDraggingWillDecelerate(decelerate)
-		}
-	}
-
-	public func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-		let headerView = appBar.headerViewController.headerView
-		if scrollView == headerView.trackingScrollView {
-			headerView.trackingScrollWillEndDragging(withVelocity: velocity, targetContentOffset: targetContentOffset)
-		}
-	}
+  public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    if scrollView == appBar.headerViewController.headerView.trackingScrollView {
+      appBar.headerViewController.headerView.trackingScrollDidScroll()
+    }
+  }
+  
+  public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+    if scrollView == appBar.headerViewController.headerView.trackingScrollView {
+      appBar.headerViewController.headerView.trackingScrollDidEndDecelerating()
+    }
+  }
 }
