02c35d0f5c8ba81005249f535e2724af458d7be5
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-15 10:50:09 -0500

    Textfields now move on another

--- FreshPlan/Sources/Modules/Register/RegisterViewController.swift
+++ FreshPlan/Sources/Modules/Register/RegisterViewController.swift
@@ -155,6 +155,14 @@ public final class RegisterViewController: UIViewController {
       .asObservable()
       .bind(to: displayNameFieldController.rx.errorText)
       .disposed(by: disposeBag)
+    
+    displayNameField.rx.controlEvent(.editingDidEndOnExit)
+      .asObservable()
+      .subscribe(onNext: { [weak self] in
+        guard let this = self else { return }
+        this.emailField.becomeFirstResponder()
+      })
+      .disposed(by: disposeBag)
   }
   
   private func prepareEmail() {
@@ -181,6 +189,14 @@ public final class RegisterViewController: UIViewController {
       .asObservable()
       .bind(to: emailFieldController.rx.errorText)
       .disposed(by: disposeBag)
+    
+    emailField.rx.controlEvent(.editingDidEndOnExit)
+      .asObservable()
+      .subscribe(onNext: { [weak self] in
+        guard let this = self else { return }
+        this.passwordField.becomeFirstResponder()
+      })
+      .disposed(by: disposeBag)
   }
   
   private func preparePassword() {
@@ -206,6 +222,14 @@ public final class RegisterViewController: UIViewController {
       .asObservable()
       .bind(to: passwordFieldController.rx.errorText)
       .disposed(by: disposeBag)
+    
+    passwordField.rx.controlEvent(.editingDidEndOnExit)
+      .asObservable()
+      .subscribe(onNext: { [weak self] in
+        guard let this = self else { return }
+        this.confirmPasswordField.becomeFirstResponder()
+      })
+      .disposed(by: disposeBag)
   }
   
   private func prepareConfirmPassword() {
@@ -218,7 +242,7 @@ public final class RegisterViewController: UIViewController {
     
     stackView.addArrangedSubview(confirmPasswordField)
     
-    confirmPasswordField.rx.controlEvent(.touchDown)
+    confirmPasswordField.rx.controlEvent(.editingDidBegin)
       .asObservable()
       .subscribe(onNext: { [weak self] in
         guard let this = self else { return }
