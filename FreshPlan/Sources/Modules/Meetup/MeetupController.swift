c9b7f7564e2aa879470fadc9e2a22002ba64ac56
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-16 20:18:37 -0500

    Added basic tableview and appbar to the controller

--- FreshPlan/Sources/Modules/Meetup/MeetupController.swift
+++ FreshPlan/Sources/Modules/Meetup/MeetupController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import MaterialComponents
+import RxSwift
+import SnapKit
 
 public final class MeetupController: UIViewController {
 	private var viewModel: MeetupViewModelProtocol!
 	private var router: MeetupRouter!
+  
+  //MARK: TableView
+  private var tableView: UITableView!
+  
+  //MARK: App Bar
+  fileprivate let appBar: MDCAppBar = MDCAppBar()
+  
+  //MARK: DisposeBag
+  private let disposeBag: DisposeBag = DisposeBag()
 	
 	public convenience init(viewModel: MeetupViewModel, router: MeetupRouter) {
 		self.init(nibName: nil, bundle: nil)
 		self.viewModel = viewModel
 		self.router = router
+    
+    addChildViewController(appBar.headerViewController)
 	}
 	
 	public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
@@ -25,8 +39,47 @@ public final class MeetupController: UIViewController {
 	public required init?(coder aDecoder: NSCoder) {
 		super.init(coder: aDecoder)
 	}
+  
+  public override var childViewControllerForStatusBarStyle: UIViewController? {
+    return appBar.headerViewController
+  }
 	
 	public override func viewDidLoad() {
 		super.viewDidLoad()
 	}
+  
+  private func prepareView() {
+    prepareTableView()
+    prepareNavigationBar()
+    appBar.addSubviewsToParent()
+  }
+  
+  private func prepareTableView() {
+    tableView = UITableView()
+    tableView.layoutMargins = .zero
+    
+    view.addSubview(tableView)
+    
+    tableView.snp.makeConstraints { make in
+      make.edges.equalTo(tableView)
+    }
+  }
+  
+  private func prepareNavigationBar() {
+    appBar.headerViewController.headerView.backgroundColor = MDCPalette.blue.tint700
+    appBar.navigationBar.tintColor = UIColor.white
+    appBar.navigationBar.titleTextAttributes = [ NSAttributedStringKey.foregroundColor: UIColor.white ]
+    
+    appBar.headerViewController.headerView.trackingScrollView = tableView
+    
+    Observable.just("Meetup")
+      .bind(to: navigationItem.rx.title)
+      .disposed(by: disposeBag)
+    
+    appBar.navigationBar.observe(navigationItem)
+  }
+  
+  deinit {
+    appBar.navigationBar.unobserveNavigationItem()
+  }
 }
