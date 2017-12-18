82371534a6955829505d9d39e81b784adbce3598
Johnny Nguyen <jnguyen1236@gmail.com>
2017-12-18 10:24:16 -0500

    Added the beginnings of Section

--- FreshPlan/Sources/Modules/MeetupDetail/MeetupDetailViewModel.swift
+++ FreshPlan/Sources/Modules/MeetupDetail/MeetupDetailViewModel.swift
@@ -9,6 +9,7 @@
 import Foundation
 import RxSwift
 import Moya
+import RxDataSources
 
 public protocol MeetupDetailViewModelProtocol {
   var title: Variable<String> { get }
@@ -40,5 +41,41 @@ public class MeetupDetailViewModel: MeetupDetailViewModelProtocol {
     return provider.rx.request(.getMeetup(meetupId))
       .asObservable()
       .map(Meetup.self, using: JSONDecoder.Decode)
+  }
+}
+
+extension MeetupDetailViewModel {
+  public struct Section {
+    public var title: String
+    public var items: [SectionItem]
+  }
+  
+  public enum SectionItem {
+    case title(order: Int, title: String)
+    case invitations(order: Int, inviteeId: Int, invitee: String, accepted: Bool)
+  }
+}
+
+extension MeetupDetailViewModel.Section: SectionModelType {
+  public typealias Items = MeetupDetailViewModel.SectionItem
+  
+  public init(original: MeetupDetailViewModel.Section, items: [Items]) {
+    self = original
+    self.items = items
+  }
+}
+
+extension MeetupDetailViewModel.SectionItem: Equatable {
+  public var order: Int {
+    switch self {
+    case .title(let order, _):
+      return order
+    case .invitations(let order, _, _, _):
+      return order
+    }
+  }
+  
+  public static func ==(lhs: MeetupDetailViewModel.SectionItem, rhs: MeetupDetailViewModel.SectionItem) -> Bool {
+    return lhs.order == rhs.order
   }
 }
