c0fd707c69600438317c8b2439c288371a077fbc
Johnny Nguyen <jnguyen1236@gmail.com>
2018-01-07 20:16:38 -0500

    Added random cell

---
+++ FreshPlan/Sources/Modules/SendInvite/Subviews/SendInviteMeetupCell.swift
@@ -0,0 +1,9 @@
+//
+//  SendInviteMeetupCell.swift
+//  FreshPlan
+//
+//  Created by Johnny Nguyen on 2018-01-07.
+//  Copyright © 2018 St Clair College. All rights reserved.
+//
+
+import Foundation
